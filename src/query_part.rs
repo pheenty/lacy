72715632da480dc89b76cbd2fe2a6ab06aaa207e
Ted Lukin <fedorlukin2006@gmail.com>
2026-08-05 09:42:10 +0700

    fix sort

--- src/query_part.rs
+++ src/query_part.rs
@@ -90,10 +90,12 @@ impl QueryPart {
                     .unwrap_or(0_i32)
                     / 2;
 
-                // sort by score, if scores are equal by alphabetical order
+                // sort descending by score
+                // else ascending, by alphabet
                 scored_dirs.sort_unstable_by(|a, b| {
                     a.score()
                         .cmp(&b.score())
+                        .reverse()
                         .then(a.directory().location().cmp(b.directory().location()))
                 });
 
